22302d51be2947baef4225ff51be80006dbcea1c
abhamayee <106445682+abhamayee@users.noreply.github.com>
2023-12-24 14:28:37 +0100

    sample controller to test

---
+++ src/main/java/com/example/deploymentonec2/DeploymentOnEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.deploymentonec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentOnEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentOnEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deploymentonec2/DeploymentOnEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploymentonec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentOnEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
